0d745a62b8ff8d3367be30af0ea0ce953f1adec9
Christian Vogl <cvogl@mpa-garching.mpg.de>
2019-08-16 14:03:04 +0200

    Add discovery magnitude to generated OBs

--- snII_cosmo_tools/ob_generation.py
+++ snII_cosmo_tools/ob_generation.py
@@ -4,14 +4,38 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 
 
+class Magnitude(object):
+    def __init__(self, value, band):
+        self.value = value
+        self.band = band
+
+    @classmethod
+    def from_row(cls, row):
+        band = row['Discovery Mag Filter']
+        value = row['Discovery Mag']
+        return cls(value, band)
+
+    @property
+    def band_simple(self):
+        band_simple = self.band.strip('-ZTF')
+        return band_simple
+
+    @property
+    def ob_user_comment(self):
+        user_comment = '{}={:.1f}'.format(self.band_simple,
+                                          round(self.value, 2))
+        return user_comment
+
+
 class OBGenerator(object):
     value_regex = re.compile('(?<=\")(?P<name>\S*)(?=\")') # noqa
 
-    def __init__(self, target_name, coords,
+    def __init__(self, target_name, coords, magnitude,
                  template_ob='data/template_obs/ob2508588.obx',
                  prefix='TOO_', dest_path='data/generated_obs'):
         self.target_name = target_name
         self.coords = coords
+        self.magnitude = magnitude
         self.prefix = prefix
         self.dest_path = dest_path
         self.template_ob = template_ob
@@ -20,7 +44,8 @@ class OBGenerator(object):
     def from_row(cls, row):
         target_name = row.name
         coords = SkyCoord(row.RA, row.DEC, unit=(u.hourangle, u.deg))
-        return cls(target_name, coords)
+        magnitude = Magnitude.from_row(row)
+        return cls(target_name, coords, magnitude)
 
     @property
     def ra(self):
@@ -50,6 +75,7 @@ class OBGenerator(object):
         ob = self.set_value(ob, 'TARGET.NAME', self.target_name)
         ob = self.set_value(ob, 'ra', self.ra)
         ob = self.set_value(ob, 'dec', self.dec)
+        ob = self.set_value(ob, 'userComments', self.magnitude.ob_user_comment)
 
         if not os.path.isdir(self.dest_path):
             os.mkdir(self.dest_path)

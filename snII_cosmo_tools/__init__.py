48c59b95afdd116530d5b86db8f036d79915d40b
Christian Vogl <cvogl@mpa-garching.mpg.de>
2019-08-19 11:54:22 +0200

    Add class for interactive target filtering

--- snII_cosmo_tools/__init__.py
+++ snII_cosmo_tools/__init__.py
@@ -3,8 +3,8 @@ from .style import (highlight_dec_cut, highlight_visibility,
                    insert_survey_links_into_df,
                    highlight_gal_lat, get_styled_html_table,
                    highlight_general_traffic_light)
-from .tns_downloader import TNSDownloader
-from .filter_targets import TargetFilter
+from .tns_downloader import TNSDownloader, TNSSpectrum, TNSDownloadError
+from .filter_targets import TargetFilter, InteractiveTargetFilter
 from .visibility import insert_max_alt_in_frame, insert_gal_lat_in_frame
 from .visualization import get_aladin_coords
 from .visibility import Visibility
--- snII_cosmo_tools/filter_targets.py
+++ snII_cosmo_tools/filter_targets.py
@@ -1,5 +1,6 @@
 import numpy as np
-from .style import get_styled_html_table
+from ipywidgets import ToggleButtons, FloatSlider, interactive
+from snII_cosmo_tools.style import get_styled_html_table
 from IPython.display import display, HTML
 
 
@@ -33,3 +34,41 @@ class TargetFilter(object):
         display(HTML(get_styled_html_table(self.targets[mask])))
 
         return self.targets[mask]
+
+
+class InteractiveTargetFilter(TargetFilter):
+    def __init__(self, targets):
+        super().__init__(targets)
+        self.obj_type = ToggleButtons(
+            options=['unclassified', 'SN II', 'SN IIP', 'SN Ia'],
+            description='Type:', disabled=False, button_style=''
+        )
+        self.max_alt = FloatSlider(
+            value=30.,
+            min=0.,
+            max=90.,
+            step=2.,
+            description='Max alt cut:',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='.1f',
+        )
+        self.mag_cut = FloatSlider(
+            value=17.,
+            min=10.,
+            max=22.,
+            step=0.25,
+            description='Max mag cut:',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='.1f',
+        )
+
+        self.interactive = interactive(self.filter_targets,
+                                       obj_type=self.obj_type,
+                                       max_alt=self.max_alt,
+                                       mag_cut=self.mag_cut)

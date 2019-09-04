3ce5dce183497a1c901387be59daa1400246f908
Christian Vogl <cvogl@mpa-garching.mpg.de>
2019-09-04 14:18:31 +0200

    Add ZTF light curve visualization

--- snII_cosmo_tools/__init__.py
+++ snII_cosmo_tools/__init__.py
@@ -4,7 +4,8 @@ from .style import (highlight_dec_cut, highlight_visibility,
                     insert_survey_links_into_df,
                     highlight_gal_lat, get_styled_html_table,
                     highlight_general_traffic_light)
-from .tns_downloader import TNSDownloader, TNSSpectrum, TNSDownloadError
+from .tns_downloader import (TNSDownloader, TNSSpectrum,
+                             TNSDownloadError, ZtfLightCurveDownloader)
 from .filter_targets import TargetFilter, InteractiveTargetFilter
 from .visibility import insert_max_alt_in_frame, insert_gal_lat_in_frame
 from .visualization import TargetVisualizer
@@ -16,7 +17,7 @@ json_keyfile_names = glob.glob('auth/*.json')
 if json_keyfile_names:
     from .archive_targets import GspreadTargetArchiver as TargetArchiver
     TargetArchiver.json_keyfile_name = json_keyfile_names[0]
-    print('Logging to google spread sheet!')
+    print('Logging to google spreadsheet!')
 else:
     print('There is no JSON keyfile in the auth folder!\nLogging to hdf file!')
     from .archive_targets import LocalTargetArchiver as TargetArchiver
--- snII_cosmo_tools/tns_downloader.py
+++ snII_cosmo_tools/tns_downloader.py
@@ -1,9 +1,16 @@
 import io
 import re
+import json
 import webbrowser
 import requests
 import pandas as pd
+import numpy as np
+from jinja2 import Template
+from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.models.sources import ColumnDataSource
+from bokeh.models import Arrow, Label, HoverTool
 from datetime import datetime, timedelta
 
 # http://astronotes.co.uk/blog/2016/01/28/Parsing-The-Transient-Name-Server.html
@@ -38,7 +45,7 @@ class TNSDownloader(object):
         if self.obj_type:
             self.query_dict['objtype[]'] = self.obj_type_keys[self.obj_type]
         if only_classified_sne:
-           self.query_dict["classified_sne"] = "1"
+            self.query_dict["classified_sne"] = "1"
 
     def query(self):
         print("Making a query!")
@@ -121,3 +128,139 @@ class TNSSpectrum(object):
         plt.xlabel('Wavelength $[\AA]$')
         plt.title('Classification spectrum')
         return fig
+
+
+class ZtfLightCurveDownloader(object):
+    base_url = 'https://lasair.roe.ac.uk/object/'
+    bands = ['g', 'r']
+    cutout_template = Template(
+        'https://lasair.roe.ac.uk/lasair/static/ztf/stamps/jpg/{{id[:3]}}/candid{{id}}.jpg'
+    )
+
+    def __init__(self, name):
+        self.name = name
+        self._json = None
+        self._non_detections = None
+        self._detections = None
+
+    @property
+    def url(self):
+        return self.base_url + self.name.replace(' ', '') + '/json'
+
+    @property
+    def json(self):
+        if not self._json:
+            self._json = json.loads(requests.get(self.url).text)
+        return self._json
+
+    @property
+    def non_detections(self):
+        if not self._non_detections:
+            non_detections = []
+            for candidate in self.json['candidates']:
+                if 'candid' not in candidate:
+                    non_detections.append(candidate)
+            non_detections = pd.DataFrame(non_detections)
+            self._non_detections = {
+                'g': non_detections[non_detections.fid == 1],
+                'r': non_detections[non_detections.fid == 2]
+            }
+        return self._non_detections
+
+    @property
+    def detections(self):
+        if not self._detections:
+            detections = []
+            for candidate in self.json['candidates']:
+                if 'candid' in candidate:
+                    detections.append(candidate)
+            detections = pd.DataFrame(detections)
+            self._detections = {
+                'g': detections[detections.fid == 1],
+                'r': detections[detections.fid == 2]
+            }
+        return self._detections
+
+    def plot(self):
+        hover = HoverTool(
+            tooltips="""
+                <img
+                    src="@imgs" height="380" alt="@imgs" width="380"
+                    style="float: left; margin: 15px 15px 15px 15px;"
+                    border="2"
+                ></img>
+        """
+        )
+
+        f = figure(x_axis_label='MJD', y_axis_label='Difference Magnitude',
+                   width=400, height=130)
+        f.add_tools(hover)
+
+        if self.best_non_detection is not None:
+            y_arrow = 0.5 * (self.best_non_detection.magpsf + self.first_detection.magpsf)
+            delta_non = str(round(self.delta_non_detection, 2)) + ' d'
+            label = Label(x=self.best_non_detection.mjd,
+                          y=y_arrow, text=delta_non, level='glyph',
+                          x_offset=10, y_offset=20, render_mode='canvas')
+            arrow = Arrow(x_start=self.first_detection.mjd,
+                          y_start=y_arrow,
+                          x_end=self.best_non_detection.mjd,
+                          y_end=y_arrow)
+            f.add_layout(arrow)
+            f.add_layout(label)
+
+        for col, band in zip(['green', 'red'], ['g', 'r']):
+            f.scatter(x=self.non_detections[band].mjd,
+                      y=self.non_detections[band].magpsf, legend='non-' + band,
+                      color=col, alpha=0.6, size=7)
+            if len(self.detections[band]) > 0:
+                source = ColumnDataSource(
+                    data=dict(x=self.detections[band].mjd,
+                              y=self.detections[band].magpsf,
+                              imgs=self.cutout_urls[band])
+                )
+                f.diamond(x='x', y='y', source=source, legend=band,
+                          color=col, size=14)
+        f.legend.click_policy = "hide"
+        f.sizing_mode = 'scale_width'
+        f.legend.location = "top_left"
+        f.y_range.flipped = True
+        return f
+
+    @property
+    def first_detection(self):
+        return pd.concat(
+            [self.detections[band] for band in self.bands]).sort_values(
+                'mjd', ascending=True).iloc[0]
+
+    @property
+    def best_non_detection(self):
+        non_detections = pd.concat(
+            [self.non_detections[band] for band in self.bands]
+        )
+        deep_mask = non_detections.magpsf > (self.first_detection.magpsf + 0.4)
+        earlier_mask = non_detections.mjd < self.first_detection.mjd
+        non_detections = non_detections[np.logical_and(deep_mask,
+                                                       earlier_mask)]
+        if len(non_detections) > 0:
+            i = np.argmin(
+                (self.first_detection.mjd - non_detections.mjd).values
+            )
+            best_non_detection = non_detections.iloc[i]
+        else:
+            best_non_detection = None
+        return best_non_detection
+
+    @property
+    def cutout_urls(self):
+        cutout_urls = {}
+        for band in self.bands:
+            ids = self.detections[band].candid
+            cutout_urls[band] = [
+                self.cutout_template.render(id=str(id)) for id in ids
+            ]
+        return cutout_urls
+
+    @property
+    def delta_non_detection(self):
+        return self.first_detection.mjd - self.best_non_detection.mjd

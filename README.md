7119423eb1ece904af225c9c1cf36764ed29cd11
Christian Vogl <cvogl@mpa-garching.mpg.de>
2019-09-10 17:04:24 +0200

    Add 2dF redshifts

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ conda activate snII_cosmo_tools
 python setup.py install
 cd snII_cosmo_tools
 ```
-Download the [redshift data](https://drive.google.com/file/d/1pMsBluOxjmcv9FVdKGG1shpPMxtieX9-/view?usp=sharing) from google drive and move it into the `redshift_data` folder.
+Download the redshift data ([glade](https://drive.google.com/file/d/1pMsBluOxjmcv9FVdKGG1shpPMxtieX9-/view?usp=sharing), [2dF](https://drive.google.com/file/d/1YtgEEe9rA0IVe1woP3dhr9hH0MU3YxWr/view?usp=sharing)) from google drive and move it into the `redshift_data` folder.
 To enable the ipyaladin widget, execute the following:
 ```
 jupyter nbextension enable --py widgetsnbextension
--- snII_cosmo_tools/redshift.py
+++ snII_cosmo_tools/redshift.py
@@ -1,24 +1,27 @@
 import numpy as np
 import pandas as pd
 import astropy.units as units
+import matplotlib.pyplot as plt
 from astropy.cosmology import Planck15
 from astropy.coordinates import SkyCoord
 from astropy.table import Table, hstack
 
 
-class GladeRedshiftCatalogue(object):
-    comp_repr = ['z', 'flag1', 'd_proj[kpc]', 'PGC', 'GWGC name',
-                 'HyperLEDA name', '2MASS name', 'SDSS-DR12 name', 'dist',
-                 'dist_err', 'flag2', 'mu', 'abs_mag']
+class RedshiftCatalogue(object):
+    comp_repr = slice(None)
 
     def __init__(self, fname, min_z=1e-2, max_z=0.5):
         self.data = pd.HDFStore(fname, mode='r').data
         self.data_description = pd.HDFStore(fname, mode='r').description
         self.data = self.data[self.data.z > min_z]
         self.data = self.data[self.data.z < max_z]
-        self.coords = SkyCoord(self.data.RA, self.data.dec,
-                               frame="icrs", unit=units.deg)
+        self.coords = self.get_sky_coords(self.data)
         self.arcsec_per_kpc = Planck15.arcsec_per_kpc_proper(self.data.z)
+
+    @staticmethod
+    def get_sky_coords(data):
+        return SkyCoord(data.RA, data.dec,
+                        frame="icrs", unit=units.deg)
 
     def find_host_z(self, target_coords, mag=0.0, max_dist_kpc=20.):
         sep = self.coords.separation(target_coords).to(units.arcsec).value
@@ -56,7 +59,7 @@ class GladeRedshiftCatalogue(object):
 
     # TODO: improve logic
     def generate_aladin_table(self, m):
-        coords = SkyCoord(m.RA, m.dec, unit=units.deg)
+        coords = self.get_sky_coords(m)
         ra = coords.ra.to_string(units.hourangle, sep=':')
         dec = [coord.split(' ')[1] for coord in coords.to_string('hmsdms')]
         dec = [
@@ -73,3 +76,34 @@ class GladeRedshiftCatalogue(object):
 
         return self.find_host_z(target_coords, mag=row['Discovery Mag'],
                                 max_dist_kpc=max_dist_kpc)
+
+    def plot(self, fig=None):
+        ra_rad = self.coords.ra.wrap_at(180 * units.deg).radian
+        dec_rad = self.coords.dec.radian
+        if not fig:
+            fig = plt.figure()
+        plt.subplot(111, projection="aitoff")
+        plt.grid(True)
+        plt.scatter(ra_rad, dec_rad, s=1.5, alpha=0.1)
+        plt.subplots_adjust(top=0.95, bottom=0.0)
+        return fig
+
+
+class GladeRedshiftCatalogue(RedshiftCatalogue):
+    short_name = 'glade'
+    comp_repr = ['z', 'flag1', 'd_proj[kpc]', 'PGC', 'GWGC name',
+                 'HyperLEDA name', '2MASS name', 'SDSS-DR12 name', 'dist',
+                 'dist_err', 'flag2', 'mu', 'abs_mag']
+
+
+class TwodFRedshiftCatalogue(RedshiftCatalogue):
+    short_name = '2dF'
+    comp_repr = ['z', 'name', 'spectra', 'ra2000',
+                 'dec2000', 'BJG', 'BJSEL', 'GALEXT',
+                 'SB_BJ', 'z_helio', 'quality', 'abemma',
+                 'Z_ABS', 'Z_EMI', 'NMBEST', 'SNR', 'mu', 'abs_mag']
+
+    @staticmethod
+    def get_sky_coords(data):
+        return SkyCoord(data.ra2000, data.dec2000,
+                        unit=(units.hourangle, units.deg))
